597343235ed25f80fa25ecae5f02d733610f1ecf
Robat Das <orvi2014@users.noreply.github.com>
2016-09-11 12:12:39 +0600

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from 'muicss/lib/react/form';
-import Input from 'muicss/lib/react/input';
-import Textarea from 'muicss/lib/react/textarea';
-import Button from 'muicss/lib/react/button';
 
-class Example extends React.Component {
-  render() {
-    return (
-      <div>
+class App extends React.Component {
+   render() {
+      return (
+         <div>
             Hello World!!!
          </div>
-    );
-  }
+      );
+   }
 }
 
-export default Example;
+export default App;
+

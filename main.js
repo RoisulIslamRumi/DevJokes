755133e3c5a2ee5a560d45f93f9710e71685e2d0
Robat Das <orvi2014@users.noreply.github.com>
2016-09-11 12:09:37 +0600

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 
-ReactDOM.render(<Example />, document.getElementById('example'));
+ReactDOM.render(<App />, document.getElementById('app'));
+
